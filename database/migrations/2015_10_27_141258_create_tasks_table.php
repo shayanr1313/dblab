9bc941ec579a9db7fc9e55577c5a57e42a8d3655
shayanr1313 <shayanr1313@gmail.com>
2017-01-17 03:01:50 +0330

    edit task table

--- database/migrations/2015_10_27_141258_create_tasks_table.php
+++ database/migrations/2015_10_27_141258_create_tasks_table.php
@@ -15,6 +15,8 @@ class CreateTasksTable extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('text');
+            $table->string('list');
             $table->timestamps();
         });
     }
